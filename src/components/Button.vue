a92a72f07c6309dbe50de5284ff30bca396f9b47
Gen1usG <812487021@qq.com>
2020-03-02 05:15:48 +0800

    新增Button FormItem通用组件

---
+++ src/components/Button.vue
@@ -0,0 +1,29 @@
+<template>
+    <button class="button">
+        <slot></slot>
+    </button>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component} from 'vue-property-decorator';
+
+    @Component
+    export default class Button extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+    .button {
+        border: none;
+        border-radius: 4px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+
+        color: #ffffff;
+        font-size: 17px;
+        width: 98px;
+    }
+</style>
---
+++ src/components/FormItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <label class="notes">
+        <span>{{name}}</span>
+        <input type="text" :placeholder="placeholder" :value="value">
+    </label>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
+
+    @Component
+    export default class FormItem extends Vue {
+        @Prop({required: true}) name!: string;
+        @Prop(String) placeholder?: string;
+        @Prop({default: ''}) value!: string;
+    }
+</script>
+
+<style scoped lang="scss">
+    .notes {
+        background-color: #E5E5E5;
+        line-height: 22px;
+        padding-left: 16px;
+        color: #333;
+
+        > input {
+            height: 40px;
+            background-color: inherit;
+            box-shadow: none;
+            border: none;
+            width: 80%;
+            margin-left: 16px;
+        }
+    }
+</style>
