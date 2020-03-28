636d6fccc5281f4f7c25b606af5d26586e2f8eb5
Gen1usG <812487021@qq.com>
2020-03-29 03:50:50 +0800

    完成统计页面的收支分类和日期美化

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -4,12 +4,12 @@
             <Tabs :data-source="dataSource" :value.sync="value"/>
             <ol>
                 <li class="items-container" v-for="(group,key) in result" :key="key">
-                    <h3>{{group.title}}<span>￥{{group.total.toString()}}</span></h3>
+                    <h3>{{beautify(group.title)}}<span>￥{{group.total.toString()}}</span></h3>
                     <ol class="item-body">
                         <li class="item-each" v-for="(item,index) in group.item" :key="index">
                             <span class="item-tag">{{tagString(item.tags)}}</span>
                             <span class="item-note">{{item.note?item.note:''}}</span>
-                            <span class="item-amount">￥{{item.numpad}}</span>
+                            <span>￥{{item.numpad}}</span>
                         </li>
                     </ol>
                 </li>
@@ -54,17 +54,18 @@
             temp.sort((a, b) => {
                 return (dayjs(b.createTime).valueOf() - dayjs(a.createTime).valueOf());
             });
+            let group = temp.filter(item => item.type === this.value);
             // [{title:2020-03-24,item:[{record1},{record2}...]},{title...}]
-            if(temp.length>0){
+            if (group.length > 0) {
                 let result: ResultItem[] = [
                     {
-                        title: dayjs(temp[0].createTime).format('YYYY-M-D'),
-                        item: [temp[0]],
-                        total: temp[0].numpad
+                        title: dayjs(group[0].createTime).format('YYYY-M-D'),
+                        item: [group[0]],
+                        total: group[0].numpad
                     }
                 ];
-                for (let i = 1; i < temp.length; i++) {
-                    const current = temp[i];
+                for (let i = 1; i < group.length; i++) {
+                    const current = group[i];
                     const lastResult = result[result.length - 1];
                     if (dayjs(current.createTime).isSame(dayjs(lastResult.title), 'date')) {
                         lastResult.item.push(current);
@@ -78,13 +79,28 @@
                     }
                 }
                 console.log(result);
+
                 return result;
             }
 
         }
 
         tagString(tag: Tag[]) {
-            return tag.length === 0 ? '无' : tag.map(item=>item.name).join(',')
+            return tag.length === 0 ? '无' : tag.map(item => item.name).join(',');
+        }
+
+        beautify(string: string) {
+            const day = dayjs(string);
+            const now = dayjs();
+            if (day.isSame(now, 'date')) {
+                return '今天';
+            } else if (day.isSame(now.subtract(1, 'day'), 'date')) {
+                return '昨天';
+            } else if (day.isSame(now.subtract(2, 'day'), 'date')) {
+                return '前天';
+            }else{
+                return string
+            }
         }
 
 
@@ -136,18 +152,30 @@
             background-color: #fff;
             padding: 9px 16px;
 
+
             span {
                 font-size: $font-size;
+            }
+
+            .item-tag {
+                display: inline-block;
+                width:15vw;
             }
 
             .item-each {
                 display: flex;
                 justify-content: space-between;
+                margin-bottom: 10px;
+
+                &:last-child {
+                    margin-bottom: 0;
+                }
             }
 
             .item-note {
                 margin-right: auto;
                 padding-left: 13px;
+                width: 65vw;
                 color: #999;
             }
 
